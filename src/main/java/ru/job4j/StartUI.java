20637576525708db24da4c81fdb76bea84e31527
Egor <yegeraskin13@gmail.com>
2020-11-22 21:29:56 +0400

    Init

---
+++ src/main/java/ru/job4j/StartUI.java
@@ -0,0 +1,7 @@
+package ru.job4j;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Validation validation = new Validation(args);
+    }
+}
---
+++ src/main/java/ru/job4j/Validation.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class Validation {
+    private final String[] args;
+    public Validation(String[] args) {
+        this.args = args;
+    }
+}
